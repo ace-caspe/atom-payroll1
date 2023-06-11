d49845987959c1a9ae1d79f87b63d57e63316992
ace-caspe <117331846+ace-caspe@users.noreply.github.com>
2023-06-11 16:25:07 +0800

    imeplemented sidebar

--- resources/views/livewire/side-bar.blade.php
+++ resources/views/livewire/side-bar.blade.php
@@ -1,10 +1,11 @@
 {{-- * NOTE: Side Bar Containter  --}}
 <div class="odecci-side-bar">
     <div class="odecci-company-logo">
-        <img src="{{ asset("/images/Odecci-logo.png") }}" alt="company_name"/>
+        <img src="{{ asset("/assets/images/Odecci-logo.png") }}" alt="company_name" width="100%" height="100%"/>
     </div>
     <ul class="odecci-nav-menu">
-        <li>Dashboard</li>
-        <li>Employee</li>
+        <li><img src="{{ asset("/assets/images/Dashboard.png") }}" alt="Dashboard" />
+            <span>Dashboard</span></li>
+        <li><img src="{{ asset("/assets/images/UserList.png") }}" alt="UserList" /><span>Employee</span></li>
     </ul>
 </div>
